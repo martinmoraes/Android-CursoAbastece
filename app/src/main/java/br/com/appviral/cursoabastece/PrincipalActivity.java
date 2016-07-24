de475fdcc189322e06d4e69b4f4444bb402a220e
Martin Morães <martin.moraes@gmail.com>
2016-07-24 17:04:17 -0300

    Adicionado o Hamburger.

--- app/src/main/java/br/com/appviral/cursoabastece/PrincipalActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/PrincipalActivity.java
@@ -1,6 +1,8 @@
 package br.com.appviral.cursoabastece;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +20,14 @@ public class PrincipalActivity extends AppCompatActivity implements NavigationVi
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_abrir, R.string.navigation_drawer_fechar);
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
     }
 
     @Override
