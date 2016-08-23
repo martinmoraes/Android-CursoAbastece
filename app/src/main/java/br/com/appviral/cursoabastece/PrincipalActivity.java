4324ae7086d3969023f172953f346593a008c448
Martin Morães <martin.moraes@gmail.com>
2016-08-23 16:11:57 -0300

    Aula 11 - Toolbar com Hamburger.

--- app/src/main/java/br/com/appviral/cursoabastece/PrincipalActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/PrincipalActivity.java
@@ -1,8 +1,11 @@
 package br.com.appviral.cursoabastece;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class PrincipalActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
@@ -11,6 +14,17 @@ public class PrincipalActivity extends AppCompatActivity implements NavigationVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.abre_gaveta, R.string.fecha_gaveta);
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.getMenu().findItem(R.id.nav_abastecimentos).setChecked(true);
