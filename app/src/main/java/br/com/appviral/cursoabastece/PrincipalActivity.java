b5eed4997b93ef3c3d1e2222298e9242bb72aac0
Martin Morães <martin.moraes@gmail.com>
2016-08-23 15:48:50 -0300

    aula 9 - Seletor de cor para o menu da gaveta.

--- app/src/main/java/br/com/appviral/cursoabastece/PrincipalActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/PrincipalActivity.java
@@ -1,13 +1,24 @@
 package br.com.appviral.cursoabastece;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class PrincipalActivity extends AppCompatActivity {
+public class PrincipalActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.getMenu().findItem(R.id.nav_abastecimentos).setChecked(true);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return true;
     }
 }
