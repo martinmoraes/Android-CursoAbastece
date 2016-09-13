8db854f2454e5042a48c9e20efdbf4be68a9bd93
Martin Morães <martin.moraes@gmail.com>
2016-09-13 13:27:47 -0300

    Personalizando ToolBar da Activity Abastecimento. Botão voltar e salvar.

--- app/src/main/java/br/com/appviral/cursoabastece/AbastecimentoActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/AbastecimentoActivity.java
@@ -1,7 +1,12 @@
 package br.com.appviral.cursoabastece;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AbastecimentoActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class AbastecimentoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abastecimento);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Abastecer");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_abastecimento_toolbar,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.salvar)
+            Toast.makeText(this, "Acionado o Salvar.", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
