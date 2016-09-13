71ffd690e16cb933b89c62e915f60aea4b3e9318
Martin Morães <martin.moraes@gmail.com>
2016-09-13 13:45:51 -0300

    Criado a activity abastece e colocado o Toolbar.

---
+++ app/src/main/java/br/com/appviral/cursoabastece/AbasteceActivity.java
@@ -0,0 +1,13 @@
+package br.com.appviral.cursoabastece;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AbasteceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abastece);
+    }
+}
