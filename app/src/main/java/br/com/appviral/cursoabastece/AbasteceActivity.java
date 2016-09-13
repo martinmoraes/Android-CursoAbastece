841a2ba93ce9583998805c6ddfd1696a12f288ec
Martin Morães <martin.moraes@gmail.com>
2016-09-13 14:22:53 -0300

    Aula 18 - Personalisando toolbar da activity abastece. Voltar e salvar.

--- app/src/main/java/br/com/appviral/cursoabastece/AbasteceActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/AbasteceActivity.java
@@ -1,7 +1,12 @@
 package br.com.appviral.cursoabastece;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AbasteceActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class AbasteceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abastece);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Abastecer");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_abastece_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.salvar)
+            Toast.makeText(this, "Clicou em Salvar.", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
