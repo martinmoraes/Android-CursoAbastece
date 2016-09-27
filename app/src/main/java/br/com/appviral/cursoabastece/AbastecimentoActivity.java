6a8e9ed69ee3413dacb179e9a8dc678b576c684d
Martin Morães <martin.moraes@gmail.com>
2016-09-27 16:33:04 -0300

    Inicializado os campos Abastece

--- app/src/main/java/br/com/appviral/cursoabastece/AbastecimentoActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/AbastecimentoActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Currency;
+
 public class AbastecimentoActivity extends AppCompatActivity {
+    Spinner mTipoCombustivelSpinner;
+    EditText mValorTotalEditText, mValorPrecoEditText, mLitrosEditText, mDataEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +33,42 @@ public class AbastecimentoActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle("Abastecer");
         }
+
+
+        // Instanciando componentes
+        mTipoCombustivelSpinner = (Spinner) findViewById(R.id.tipoCombustivelSpinner);
+        mValorTotalEditText = (EditText) findViewById(R.id.valorTotalEditText);
+        mValorPrecoEditText = (EditText) findViewById(R.id.valorPrecoEditText);
+        mLitrosEditText = (EditText) findViewById(R.id.litrosEditText);
+        mDataEditText = (EditText) findViewById(R.id.dataEditText);
+        inicializaComponentes();
+
+    }
+
+    private void inicializaComponentes() {
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.combustivel, android.R.layout.simple_spinner_dropdown_item);
+        mTipoCombustivelSpinner.setAdapter(adapter);
+
+        String simbolo = NumberFormat.getInstance().getCurrency().getSymbol();
+        mValorTotalEditText.setHint(simbolo);
+        mValorPrecoEditText.setHint(simbolo);
+
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        mDataEditText.setText(dateFormat.format(Calendar.getInstance().getTime()));
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_abastecimento_toolbar,menu);
+        getMenuInflater().inflate(R.menu.activity_abastecimento_toolbar, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.salvar)
+        if (item.getItemId() == R.id.salvar)
             Toast.makeText(this, "Acionado o Salvar.", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
