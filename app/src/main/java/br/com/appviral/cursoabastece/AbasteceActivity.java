0d8a45565a04f8324dabf4d82b2499ef4effed7c
Martin Morães <martin.moraes@gmail.com>
2016-09-27 17:04:33 -0300

    Aula 20 - Inicializando os componentes da Activity Abastecer.

--- app/src/main/java/br/com/appviral/cursoabastece/AbasteceActivity.java
+++ app/src/main/java/br/com/appviral/cursoabastece/AbasteceActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Calendar;
+
 public class AbasteceActivity extends AppCompatActivity {
+
+    Spinner mCombustivelSpinner;
+    EditText mTotalEditText, mPrecoEditText, mLitrosEditText, mDataEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,30 @@ public class AbasteceActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle("Abastecer");
         }
+
+
+
+        //Vincular os componentes
+        mCombustivelSpinner = (Spinner) findViewById(R.id.combustivelSpinner);
+        mTotalEditText = (EditText) findViewById(R.id.totalEditText);
+        mPrecoEditText = (EditText) findViewById(R.id.precoEditText);
+        mLitrosEditText = (EditText) findViewById(R.id.litrosEditText);
+        mDataEditText = (EditText) findViewById(R.id.dataEditText);
+        inicializaComponentes();
+
+    }
+
+    private void inicializaComponentes(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.combustivel, android.R.layout.simple_spinner_dropdown_item);
+        mCombustivelSpinner.setAdapter(adapter);
+
+        String simbolo = NumberFormat.getInstance().getCurrency().getSymbol();
+        mTotalEditText.setHint(simbolo);
+        mPrecoEditText.setHint(simbolo);
+
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        mDataEditText.setText(dateFormat.format(Calendar.getInstance().getTime()));
+
     }
 
     @Override
